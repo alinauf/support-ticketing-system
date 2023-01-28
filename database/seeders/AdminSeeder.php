82fe054d40f7903d8d173540bc31a1efefde6099
Nauf <naufpsychic@gmail.com>
2023-01-28 20:07:03 +0500

    Initial Commit :tada:

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'info@codingmonkeys.nl',
+            'password' => Hash::make('password'),
+            'is_admin' => true,
+        ]);
+    }
+}
